61b6b79c5bd9390a315fd93328e634e9245d7e9e
Tonko27 <tonkotsu4127@gmail.com>
2021-10-10 09:49:30 +0000

    first commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,6 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
