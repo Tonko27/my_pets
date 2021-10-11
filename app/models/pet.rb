ff374921ec30cc4655175d7ded337bcec50571b2
Tonko27 <tonkotsu4127@gmail.com>
2021-10-11 14:36:09 +0000

    [Add]index,edit,destroy

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,9 @@
 class Pet < ApplicationRecord
   belongs_to :customer
+  
   attachment :image#gem:refileのルール
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions#gem:active_hashに必要な記述
   belongs_to_active_hash :gender
   belongs_to_active_hash :category
   belongs_to_active_hash :age
