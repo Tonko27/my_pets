ec6001adf15c8fcb169e48f7fa34ddd36769f8b3
Tonko27 <tonkotsu4127@gmail.com>
2021-10-12 04:57:35 +0000

    [Add]devise,edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name, :email, :encrypted_password, :postalcode, :prefecture_id, :city, :building, :phone, :is_deleted])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :name, :email, :encrypted_password, :postalcode, :prefecture_id, :city, :building, :phone, :is_deleted])
   end
 end
