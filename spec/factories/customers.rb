ca61bd73fad64940714f9ca31af1eaa942b0bd4d
Tonko27 <tonkotsu4127@gmail.com>
2021-11-05 11:29:58 +0000

    [Update]rspec

---
+++ spec/factories/customers.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :customer do
+    name { 'pets_test' }
+    nickname { 'pets_test' }
+    email { 'pets@tes' }
+    postalcode { '123-4567' }
+    prefecture_id { '1' }
+    city { 'test' }
+    building { 'test' }
+    phone { '123-456-789' }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,9 +1,73 @@
-RSpec.describe "Customer", type: :model do
-  it "メールアドレス、ニックネームがある場合、有効である" do
-  Customer = Customer.new(
-          email:  "pets@1",
-          nickname: "pets",
-  )
-  expect(customer).to be_valid
+require 'rails_helper'
+
+RSpec.describe 'Customerモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { customer.valid? }
+
+    let!(:other_customer) { create(:customer) }
+    let(:customer) { build(:customer) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        customer.name = ''
+        is_expected.to eq false
+      end
+    end
+    context 'emailカラム' do
+      it '空欄でないこと' do
+        customer.email = ''
+        is_expected.to eq false
+      end
+    end
+    context 'passwordカラム' do
+      it '空欄でないこと' do
+        customer.password = ''
+        is_expected.to eq false
+      end
+    end
+    context 'nicknameカラム' do
+      it '空欄でないこと' do
+        customer.nickname = ''
+        is_expected.to eq false
+      end
+    end
+    context 'postalcodeカラム' do
+      it '空欄でないこと' do
+        customer.postalcode = ''
+        is_expected.to eq false
+      end
+    end
+    context 'prefecture_idカラム' do
+      it '空欄でないこと' do
+        customer.prefecture_id = ''
+        is_expected.to eq false
+      end
+    end
+    context 'cityカラム' do
+      it '空欄でないこと' do
+        customer.city = ''
+        is_expected.to eq false
+      end
+    end
+    context 'buildingカラム' do
+      it '空欄でないこと' do
+        customer.building = ''
+        is_expected.to eq false
+      end
+    end
+    context 'phoneカラム' do
+      it '空欄でないこと' do
+        customer.phone = ''
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Petモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Customer.reflect_on_association(:pets).macro).to eq :has_many
+      end
+    end
   end
 end
